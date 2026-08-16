eae2bd14b04a5735b68a2e8e053cd709f3000858
bickyverma <183171508+bickyverma@users.noreply.github.com>
2026-08-16 11:10:05 +0530

    Time: 4 ms (75.68%) | Memory: 114.7 MB (82.43%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.java
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i : stones){
             arr[i % 3]++;
         }
-        if(arr[0] %2 == 0){
+        if(arr[0] % 2 == 0){
             return arr[1] > 0 && arr[2] > 0;
         }
         return Math.abs(arr[1] - arr[2]) > 2;
